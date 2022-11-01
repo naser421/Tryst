34a4b5e79565c999d71dc7e59ff8e87102c018ae
Naser Suleiman <nasersuleiman421@gmail.com>
2022-11-01 13:42:49 -0700

    fixed menu

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const options = {
           // Close the dropdown menu if the user clicks outside of it
           window.onclick = function(event) {
             if (!event.target.matches('.dropbtn')) {
-              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var dropdowns = document.getElementsByClassName("dropdown-content-event");
               var i;
               for (i = 0; i < dropdowns.length; i++) {
                 var openDropdown = dropdowns[i];
